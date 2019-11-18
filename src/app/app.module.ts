9c0c92c420d733b866183a6cc9f09643b914724b
Pierre-Henri <etu35397@henallux.be>
2019-11-18 10:11:45 +0100

    premierCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasketComponent } from './basket/basket.component';
+import { ItemsListComponent } from './items-list/items-list.component';
+import { BasketSummaryComponent } from './basket-summary/basket-summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasketComponent,
+    ItemsListComponent,
+    BasketSummaryComponent
   ],
   imports: [
     BrowserModule,
